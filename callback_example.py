1bb73b9d5bf1b7fb852dab40f9b2241a8a841461
zhuowenzhao <zwenzhao11@gmail.com>
2023-04-10 13:34:43 -0700

    updated example code

--- callback_example.py
+++ callback_example.py
@@ -1,12 +1,17 @@
-from dash import Dash, html
+from dash import Dash, html, Input, Output
 from dash_component_editor import JSONParameterEditor
 
 # data
 component_kwargs = {"gui_parameters": [{"type": "int", "name": "num-tree", "title": "Number of Trees", "param_key": "n_estimators", "value": "30"}, 
-                                       {"type": "int", "name": "tree-depth", "title": "Tree Depth", "param_key": "max_depth", "value": "8"}]}
+                                       {"type": "dropdown", "name": "dropdown-menu", "title": "Choose a City", "param_key": "city", 
+                                        "options": [{'label': 'New York City', 'value': 'New York City'}, {'label': 'Montreal', 'value': 'Montreal'},
+                                                   {'label': 'San Francisco', 'value': 'San Francisco'}], "value": "'Montreal"},
+                                       {"type": "slider", "name": "slider-id", "title": "Fahrenheit Range", "param_key": "f_temp", "min": 0, "max": 10,
+                                        "step": None, "marks": {0: '0°F', 3: '3°F', 5: '5°F', 7.65: '7.65°F', 10: '10°F'}, "value":5},
+                                       ]
+                    }
 
-
-# Set up Dash server and web layouts
+# set up Dash server and web layouts
 app = Dash(__name__)
 app.layout = html.Div ([html.Button('Show GUI', id='button', n_clicks=0),
                         html.Div(id='gui-layout', children=[])
@@ -27,3 +32,5 @@ def show_gui_layouts(n_clicks):
     
     return [html.H5("GUI Layout", className="card-title"), item_list]
 
+
+app.run_server(host='0.0.0.0', port=8050, debug=True)
